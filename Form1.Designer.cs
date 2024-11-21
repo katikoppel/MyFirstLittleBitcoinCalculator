e4dfe9e4c188c4eaeafa83713ddfe1621fe210f8
opilane <katikoppel1995@gmail.com>
2024-11-21 20:10:45 +0200

    ADDED EUR AM RICH

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace BitcoinCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.currencyselector = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bitcoinamountinput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tulemuslabel = new System.Windows.Forms.Label();
+            this.resultlabel = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // currencyselector
+            // 
+            this.currencyselector.BackColor = System.Drawing.Color.Violet;
+            this.currencyselector.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.currencyselector.FormattingEnabled = true;
+            this.currencyselector.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "EEK"});
+            this.currencyselector.Location = new System.Drawing.Point(55, 99);
+            this.currencyselector.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.currencyselector.Name = "currencyselector";
+            this.currencyselector.Size = new System.Drawing.Size(200, 26);
+            this.currencyselector.TabIndex = 0;
+            this.currencyselector.Text = "Vali valuuta";
+            this.currencyselector.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Cyan;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button1.Location = new System.Drawing.Point(55, 138);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ARVUTA KURSS";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bitcoinamountinput
+            // 
+            this.bitcoinamountinput.BackColor = System.Drawing.Color.Cyan;
+            this.bitcoinamountinput.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.bitcoinamountinput.Location = new System.Drawing.Point(55, 64);
+            this.bitcoinamountinput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bitcoinamountinput.Name = "bitcoinamountinput";
+            this.bitcoinamountinput.Size = new System.Drawing.Size(200, 23);
+            this.bitcoinamountinput.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label1.Location = new System.Drawing.Point(52, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sisesta Bitcoini arv";
+            // 
+            // tulemuslabel
+            // 
+            this.tulemuslabel.AutoSize = true;
+            this.tulemuslabel.BackColor = System.Drawing.Color.Transparent;
+            this.tulemuslabel.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.tulemuslabel.ForeColor = System.Drawing.Color.Lime;
+            this.tulemuslabel.Location = new System.Drawing.Point(52, 202);
+            this.tulemuslabel.Name = "tulemuslabel";
+            this.tulemuslabel.Size = new System.Drawing.Size(70, 23);
+            this.tulemuslabel.TabIndex = 4;
+            this.tulemuslabel.Text = "Tulemus";
+            this.tulemuslabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // resultlabel
+            // 
+            this.resultlabel.BackColor = System.Drawing.Color.Fuchsia;
+            this.resultlabel.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultlabel.ForeColor = System.Drawing.Color.Lime;
+            this.resultlabel.Location = new System.Drawing.Point(55, 229);
+            this.resultlabel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.resultlabel.Name = "resultlabel";
+            this.resultlabel.Size = new System.Drawing.Size(200, 50);
+            this.resultlabel.TabIndex = 5;
+            this.resultlabel.Text = "POLE";
+            this.resultlabel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.resultlabel.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1067, 779);
+            this.Controls.Add(this.resultlabel);
+            this.Controls.Add(this.tulemuslabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bitcoinamountinput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.currencyselector);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "BitcoinCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox currencyselector;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox bitcoinamountinput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label tulemuslabel;
+        private System.Windows.Forms.TextBox resultlabel;
+    }
+}
+
